d3a6125c57838760bd79f995f711d737cfaa8b15
davidson-vs <davidson.vieiras27@gmail.com>
2024-09-15 21:28:23 -0300

    Tela de cadastro criada (apenas o visual).

--- app/frontend/project_views/src/views/cadastroUser.jsx
+++ app/frontend/project_views/src/views/cadastroUser.jsx
@@ -15,10 +15,10 @@ const CadastroUser = (props) => {
 
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
-    const [area, setArea] = useState('')
     const [isGestor, setIsGestor] = useState('N')
     const [idGestor, setGestor] = useState(null)
     const [empresa, setEmpresa] = useState(null)
+    const [password, setPassword] = useState('')
 
 
     return (
@@ -40,6 +40,17 @@ const CadastroUser = (props) => {
                         onChange={(e)=>setUsername(e.target.value)}
                         is_required={true}
                     />
+                    
+                    <SelectBoxComponent
+                        label="Empresa"
+                        name="empresa-funcionario"
+                        onChange={(e)=>setEmpresa(e.target.value)}
+                        required={true}
+                        options={[{value: 1, label: "area 1"}, {value: 2, label: "area 2"}, {value: 3, label: "area 3"}]}
+                    />
+                </div>
+
+                <div className="inputs-row">
                     <InputComponent 
                         type="email"
                         labelText="Email"
@@ -50,33 +61,43 @@ const CadastroUser = (props) => {
                         onChange={(e)=>setEmail(e.target.value)}
                         is_required={true}
                     />
-                    <SelectBoxComponent
-                        label="Empresa"
-                        name="empresa-funcionario"
-                        onChange={(e)=>setArea(e.target.value)}
-                        required={true}
-                        options={[{value: 1, label: "area 1"}, {value: 2, label: "area 2"}, {value: 3, label: "area 3"}]}
+                    <InputComponent 
+                        type="password"
+                        labelText="Senha"
+                        name="pwd-cadastro"
+                        placeholder="Digite seu e-mail"
+                        value={password}
+                        maxLength="16"
+                        onChange={(e)=>setPassword(e.target.value)}
+                        is_required={true}
                     />
                 </div>
+
                 <div className="inputs-row">
-                <RadioInput
-                        label="Você é um gestor?"
-                        options={[
-                            { id: 'radio1', name: 'radioGroup', value: 'true', label: 'Sim' },
-                            { id: 'radio2', name: 'radioGroup', value: 'false', label: 'Não' }
-                            
-                        ]}
-                        selectedValue={isGestor}
-                        onChange={(e) => setIsGestor(e.target.value)}
-                    />
-                <SelectBoxComponent
-                        label="Gestor"
-                        name="gestor-funcionario"
-                        onChange={(e)=>setGestor(e.target.value)}
-                        required={true}
-                        options={[{value: 1, label: "Gestor 1"}, {value: 2, label: "Gestor 2"}, {value: 3, label: "Gestor 3"}]}
-                    />
+                    <RadioInput
+                            label="Você é um gestor?"
+                            options={[
+                                { id: 'radio1', name: 'radioGroup', value: 'S', label: 'Sim' },
+                                { id: 'radio2', name: 'radioGroup', value: 'N', label: 'Não' }
+                                
+                            ]}
+                            selectedValue={isGestor}
+                            onChange={(e) => setIsGestor(e.target.value)}
+                        />
+                    { 
+                    isGestor === 'N' ? 
+                    <SelectBoxComponent
+                            label="Gestor"
+                            name="gestor-funcionario"
+                            onChange={(e)=>setGestor(e.target.value)}
+                            required={true}
+                            options={[{value: 1, label: "Gestor 1"}, {value: 2, label: "Gestor 2"}, {value: 3, label: "Gestor 3"}]}
+                        /> : <></>
+                    }
+                
                 </div>
+                <br />
+                <button type="submit">Casdastrar-se</button>
             </div>
         </form>
     ) 
